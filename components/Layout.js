db366397ddafdb38f6a40f26e91c075cc18e297d
dvalian <zlatoborodyi@gmail.com>
2019-06-28 15:59:30 +0300

    refactor styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
+import { LayoutStyles, GlobalStyles } from './Layout.styles'
+
 const Layout = ({ children, title, description }) => (
     <div>
         <Head>
@@ -18,39 +20,8 @@ const Layout = ({ children, title, description }) => (
             {children}
         </div>
 
-        <style jsx>{`
-            .container {
-                max-width: 800px;
-                margin: 0 auto;
-                background: #f6f6ef;
-            }
-
-            nav {
-                background: #f60;
-                padding: 1rem;
-            }
-
-            nav > * {
-                display: inline-block;
-                color: black;
-            }
-
-            nav a {
-                text-decoration: none;
-            }
-
-            nav .main-title {
-                font-weight: bold;
-            }
-        `}</style>
-
-        <style global jsx>{`
-            body {
-                background: white;
-                font-size: 14px;
-                font-family: Verdana, Geneva, sans-serif;
-            }
-        `}</style>
+        <LayoutStyles />
+        <GlobalStyles />
     </div>
 )
 
